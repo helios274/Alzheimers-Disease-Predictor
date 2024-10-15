e413a4aebff5b54098280d7462fba33f6c31ca4c
Adithya Prasad <adithyasridharprasad@gmail.com>
2024-10-15 22:45:00 +0530

    First commit

---
+++ predictor/index.py
@@ -0,0 +1,25 @@
+import os
+import pickle
+import numpy as np
+
+
+features = ['Age', 'BMI', 'AlcoholConsumption', 'CholesterolTotal',
+            'CholesterolLDL', 'CholesterolHDL', 'CholesterolTriglycerides',
+            'PhysicalActivity', 'DietQuality', 'SleepQuality', 'MMSE',
+            'FunctionalAssessment', 'ADL', 'MemoryComplaints', 'BehavioralProblems']
+
+print(os.getcwd())
+with open('predictor/models/model_gbc_v2-3.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+
+def make_prediction(data):
+
+    input_data = [data.get(f) for f in features]
+
+    input_data = np.array(input_data).reshape(1, -1)
+
+    proba = model.predict_proba(input_data)[0].tolist()[1]
+    proba = round(proba*100, 2)
+
+    return proba
